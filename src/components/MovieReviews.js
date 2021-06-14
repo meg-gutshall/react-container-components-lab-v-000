c6393f807dbb02c56c57550b0c87e842709a74f8
Meg Gutshall <meghan.gutshall@gmail.com>
2021-06-14 15:50:25 -0400

    commit tree a46276ae86cf2bdd719e285861fe23aade6e19c7

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -3,21 +3,42 @@ import React from "react";
 const MovieReviews = ({reviews}) => {
   return (
     <div className="review-list">
-      { reviews.map((review, idx) => <Review review={review} key={idx} />) }
+      { reviews.map(Review) }
     </div>
   )
 }
 
-const Review = ({review, idx}) => {
+const Review = ({link, headline, byline, publication_date, summary_short, idx}) => {
   return (
     <div key={idx} className="review">
-      <a href={review.link.url} target="_blank" rel="noopener noreferrer"><h1>{review.headline}</h1></a>
-      {review.opening_date !== null ? <h2>Opening {review.opening_date}</h2> : <p>Opening date TBD</p>}
-      <p>Author: {review.byline}, Published: {review.publication_date}</p>
-      {review.multimedia !== null ? <a href={review.link.url} target="_blank" rel="noopener noreferrer"><img src={review.multimedia.src} alt={review.display_title} /></a> : <p>No image</p>}
-      <p>{review.summary_short}</p><br />
+      <a href={link.url} target="_blank" rel="noopener noreferrer"><h1>{headline}</h1></a>
+      <p>Author: {byline}, Published: {publication_date}</p>
+      <p>{summary_short}</p><br />
     </div>
   )
 }
 
 export default MovieReviews
+
+
+// import React from "react";
+
+// const MovieReviews = ({reviews}) => {
+//   return (
+//     <div className="review-list">
+//       { reviews.map((review, idx) => <Review review={review} key={idx} />) }
+//     </div>
+//   )
+// }
+
+// const Review = ({review, idx}) => {
+//   return (
+//     <div key={idx} className="review">
+//       <a href={review.link.url} target="_blank" rel="noopener noreferrer"><h1>{review.headline}</h1></a>
+//       <p>Author: {review.byline}, Published: {review.publication_date}</p>
+//       <p>{review.summary_short}</p><br />
+//     </div>
+//   )
+// }
+
+// export default MovieReviews
